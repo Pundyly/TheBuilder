57cbcce63a5f92a7d0796986a33dbc62e4542e2e
Pundyly <148798374+Pundyly@users.noreply.github.com>
2025-01-09 23:44:34 +0300

    Upload a version 0.0.1
    yeas😒

---
+++ TheBuilder/TheBuilder/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBuilder
+{
+    class Program
+    {
+        public int width, height, tmp_height, heightOfFoundation, tmp_heightOfFoundation;
+        public string doAHouse, temp;
+        static void Main(string[] args)
+        {
+            var debil = new Program();
+            while (true)
+            {
+                Console.WriteLine("Do a house?(y/n): ");
+                debil.doAHouse = Console.ReadLine();
+                switch (debil.doAHouse)
+                {
+                    case "y":
+                        debil.BuildAHouse();
+                        break;
+                    case "n":
+                        Console.WriteLine("Okey, D.");
+                        break;
+                    default:
+                        Console.WriteLine("ERROR: YOU ARE DUMB!");
+                        break;
+                }
+            }
+        }
+
+        public void BuildAHouse()
+        {
+            //questions
+            Console.WriteLine("Width: ");
+            width = int.Parse(Console.ReadLine());
+            Console.WriteLine("Height: ");
+            height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Height of foundation: ");
+            heightOfFoundation = int.Parse(Console.ReadLine());
+
+            //build
+            temp = string.Concat(Enumerable.Repeat("#", width));
+            Console.WriteLine(temp);
+            tmp_height = height - 1;
+            tmp_height = tmp_height - heightOfFoundation;
+            while( tmp_height > 0)
+            {
+                temp = " " + "#" + string.Concat(Enumerable.Repeat(" ", width - 4)) + "#" + " ";
+                Console.WriteLine(temp);
+                tmp_height--;
+            }
+
+            temp = string.Concat(Enumerable.Repeat("#", width));
+            tmp_heightOfFoundation = heightOfFoundation;
+
+            while (tmp_heightOfFoundation > 0)
+            {
+                temp = " " + "#" + string.Concat(Enumerable.Repeat("#", width - 4)) + "#" + " ";
+                Console.WriteLine(temp);
+                tmp_heightOfFoundation--;
+            }
+            Console.WriteLine("Sucksfull!");
+        }
+    }
+}
